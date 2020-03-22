9351fee7cb038f6bdeb3795f7a24855e47a9dbf0
mwong01 <mattw90@gmail.com>
2020-03-21 20:36:56 -0400

    initial commit

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: REPLACE_WITH_YOURS,
+  authDomain: REPLACE_WITH_YOURS,
+  databaseURL: REPLACE_WITH_YOURS,
+  projectId: REPLACE_WITH_YOURS,
+  storageBucket: REPLACE_WITH_YOURS,
+  messagingSenderId: REPLACE_WITH_YOURS,
+  appId: REPLACE_WITH_YOURS
+}
