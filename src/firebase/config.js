d4f75ce6763eb70af190cd6ab0b6913c7d908f47
mwong01 <mattw90@gmail.com>
2020-03-21 20:41:58 -0400

    set up firebase context

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  apiKey: REPLACE_WITH_YOURS,
-  authDomain: REPLACE_WITH_YOURS,
-  databaseURL: REPLACE_WITH_YOURS,
-  projectId: REPLACE_WITH_YOURS,
-  storageBucket: REPLACE_WITH_YOURS,
-  messagingSenderId: REPLACE_WITH_YOURS,
-  appId: REPLACE_WITH_YOURS
+  apiKey: "AIzaSyCvsq05fELUQ8pl_3dt22azpTL85b-YgKw",
+  authDomain: "pwa-firebase-f5036.firebaseapp.com",
+  databaseURL: "pwa-firebase-f5036.firebaseio.com",
+  projectId: "pwa-firebase-f5036",
+  storageBucket: "pwa-firebase-f5036.appspot.com",
+  messagingSenderId: "372223683088",
+  appId: "1:372223683088:web:c7289d16b22816685bb895"
 }
