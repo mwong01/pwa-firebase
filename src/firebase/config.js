730d16981c573a0b4c2ff5642aef40b3622f398a
mwong01 <mattw90@gmail.com>
2020-03-22 00:25:35 -0400

    finalize lists component

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 export const firebaseConfig = {
-  apiKey: "AIzaSyCvsq05fELUQ8pl_3dt22azpTL85b-YgKw",
-  authDomain: "pwa-firebase-f5036.firebaseapp.com",
-  databaseURL: "pwa-firebase-f5036.firebaseio.com",
+  apiKey: process.env.apiKey,
+  authDomain: process.env.apiKey,
+  databaseURL: "https://pwa-firebase-f5036.firebaseio.com",
   projectId: "pwa-firebase-f5036",
   storageBucket: "pwa-firebase-f5036.appspot.com",
   messagingSenderId: "372223683088",
   appId: "1:372223683088:web:c7289d16b22816685bb895"
-}
+};
